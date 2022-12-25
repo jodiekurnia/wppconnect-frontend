78e73d461e6fd6a42647832cede6929b6b2fd305
Jodie Kurnia <54063383+jodiekurnia@users.noreply.github.com>
2022-12-25 17:20:14 +0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.IP_SERVER = "http://localhost:21465/api/";
-window.IP_SOCKET_IO = "http://localhost:21465/";
+window.IP_SERVER = "http://waserver:21465/api/";
+window.IP_SOCKET_IO = "http://waserver:21465/";
